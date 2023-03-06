7e59ab2f7d7f0973b90178dbb3f244f82f527b97
Rajat Jha <78417819+rajatcj@users.noreply.github.com>
2023-03-07 02:38:08 +0545

    Update [...id].ts

--- pages/api/[...id].ts
+++ pages/api/[...id].ts
@@ -31,7 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         });
 
     try {
-        getUser = await axios(`https://api.lanyard.rest/v1/users/${userId}`);
+        getUser = await axios(`https://discorduserapi.rajatcj.com.np/v1/users/${userId}`);
     } catch (error: any) {
         if (error.response.data && error.response.data.error.message)
             return res
